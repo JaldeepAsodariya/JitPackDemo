a0d53ba60310fafe6bedfc8daedfe85dfcdc67e8
Jaldeep Asodariya <jdpatel.android@gmail.com>
2019-03-27 17:01:27 +0530

    Initial commit

---
+++ jitpacklibrary/src/main/java/com/mind/jitpacklibrary/JitPackActivity.kt
@@ -0,0 +1,11 @@
+package com.mind.jitpacklibrary
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class JitPackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lo_activity_jitpack)
+    }
+}
